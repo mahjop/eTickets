48034ce57f3cee52c800d1721c906709c1430649
mahjop <128293864+mahjop@users.noreply.github.com>
2023-04-28 12:32:38 +0300

    16u

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -58,6 +58,8 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            //saad database
+            AppDbInitializer.Saad(app);
         }
     }
 }
